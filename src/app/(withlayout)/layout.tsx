e281282c5b093db1919136a1b3c003d4e938bb96
noorhossain80901~ <noorsan809@gmail.com>
2023-09-25 15:13:18 +0600

    [dashboard-add]

---
+++ src/app/(withlayout)/layout.tsx
@@ -0,0 +1,15 @@
+import Contents from "@/components/ui/Contents";
+import Sidebar from "@/components/ui/Sidebar";
+import { Layout } from "antd";
+import React from "react";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Layout hasSider>
+      <Sidebar />
+      <Contents>{children}</Contents>
+    </Layout>
+  );
+};
+
+export default DashboardLayout;
---
+++ src/components/ui/Contents.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import type { MenuProps } from "antd";
+import { Breadcrumb, Layout, theme } from "antd";
+
+const { Content } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const Contents = ({ children }: { children: React.ReactNode }) => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Content style={{ minHeight: "100vh", margin: "0 16px" }}>
+      <Breadcrumb style={{ margin: "16px 0" }}>
+        <Breadcrumb.Item>User</Breadcrumb.Item>
+        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+      </Breadcrumb>
+      <div
+        style={{ padding: 24, minHeight: "100%", background: colorBgContainer }}
+      >
+        {children}
+      </div>
+    </Content>
+  );
+};
+
+export default Contents;
